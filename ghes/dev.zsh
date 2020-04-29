d28e2271b4f8ff7ebb3f53ca5f66c2f476924897
yaananth (Yash) <yaananth@github.com>
2020-04-29 16:57:12 -0400

    update

--- ghes/dev.zsh
+++ ghes/dev.zsh
@@ -1,2 +1,8 @@
 # ssh to ghub enterprise dev 
 alias sshdev="ssh -A build@yaananth-15881764793.service.bpdev-us-east-1.github.net"
+alias ghesbuild="./chroot-build.sh"
+alias ghesstart="./chroot-start.sh"
+alias ghesconfig="./chroot-configure.sh"
+alias ghesssh="./chroot-ssh.sh"
+alias ghesproxy="sudo update-reverse-proxy"
+alias ghesreset="./chroot-stop.sh;./chroot-reset.sh"
