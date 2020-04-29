84efabb3efd00cd0e9b0e28524a7545dbe587ef8
yaananth (Yash) <yaananth@github.com>
2020-04-29 15:18:47 -0400

    updates

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,9 +1,9 @@
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 
-fpath=($ZSH/functions $fpath)
+fpath=($ZSHDOTFILES/functions $fpath)
 
-autoload -U $ZSH/functions/*(:t)
+autoload -U $ZSHDOTFILES/functions/*(:t)
 
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
