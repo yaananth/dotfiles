54ed54c5023b95e8cf3fd3415d06f54b2ec1ce0c
yaananth (Yash) <yaananth@github.com>
2020-05-19 14:15:44 -0400

    update

--- dev/devnext.zsh
+++ dev/devnext.zsh
@@ -1,5 +1,4 @@
-alias devnext="cd ~/Developer/devnext/src/;./init.sh"
+alias devnext="cd ~/Developer/devnext/src/;./init.sh;"
 export AzureSubscriptionId="8a53cb9a-a3a5-4602-aa2d-8c171edde3c7"
 export DOCKER_CERT_PATH=/Users/yaananth/.minikube/certs
 export DOCKER_TLS_VERIFY=1
-export DOCKER_HOST=tcp://192.168.153.128:2376
