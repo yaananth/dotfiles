817363f0cc4da0da87c85ca8dfbb2f390f79b794
yaananth (Yash) <yaananth@github.com>
2020-06-05 15:56:51 -0400

    changes

--- dev/devnext.zsh
+++ dev/devnext.zsh
@@ -1,4 +1,2 @@
 alias devnext="cd ~/Developer/devnext/src/;./init.sh;"
 export AzureSubscriptionId="8a53cb9a-a3a5-4602-aa2d-8c171edde3c7"
-export DOCKER_CERT_PATH=/Users/yaananth/.minikube/certs
-export DOCKER_TLS_VERIFY=1
--- ghes/dev.zsh
+++ ghes/dev.zsh
@@ -1,4 +1,5 @@
 # ssh to ghub enterprise dev 
-alias sshdev="ssh -A build@yaananth-15885995988.service.bpdev-us-east-1.github.net"
+alias sshdev="ssh -A build@yaananth-3b414ed53.service.bpdev-us-east-1.github.net"
+alias sshghedev="ssh -p122 admin@yaananth-061c10f85251cc197.gh-quality.net"
 alias octofapi="security find-generic-password -wa octofactory  | pbcopy | echo '=> Octofactory API key copied to pasteboard.'"
 alias octoflogin="echo 'docker login octofactory.githubapp.com' | pbcopy | echo '=> docker login octofactory.githubapp.com copied to pasteboard.'"
