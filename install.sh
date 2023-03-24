716fadc9a408fc88152986518411bcfc8124cde3
Yashwanth Anantharaju <yaananth@github.com>
2023-03-24 11:16:48 -0400

    fix github cli

--- install.sh
+++ install.sh
@@ -11,10 +11,35 @@ if [ ! -d ~/.oh-my-zsh ]; then
     # for vscode, do "terminal.integrated.shell.linux": "/usr/bin/zsh", so that it's default
 fi
 
+# check if node is installed
+if [ ! -f /usr/local/bin/node ]; then
+    echo "Installing node"
+    curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+    sudo apt-get install nodejs -y
+fi
+
+# check node version, make sure it's at least 19, if not, install it
+NODE_VERSION=$(node --version)
+NODE_VERSION_MAJOR=$(echo $NODE_VERSION | cut -d '.' -f 1 | cut -d 'v' -f 2)
+NODE_VERSION_MINOR=$(echo $NODE_VERSION | cut -d '.' -f 2)
+
+if [ $NODE_VERSION_MAJOR -lt 19 ]; then
+    echo "Node version is less than 19, installing node 19"
+    sudo apt-get remove nodejs npm -y
+    curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+    sudo apt-get install nodejs -y
+fi
+
+# install npm if it doesn't exist
+if [ ! -f /usr/local/bin/npm ]; then
+    echo "Installing npm"
+    sudo apt-get install npm -y
+fi
+
 # install github-copilot-cli if it doesn't exist
 if [ ! -f /usr/local/bin/github-copilot-cli ]; then
     echo "Installing github-copilot-cli"
-    npm install -g @githubnext/github-copilot-cli
+    sudo npm install -g @githubnext/github-copilot-cli
 fi
 
 # remove .zshrc if it exists
